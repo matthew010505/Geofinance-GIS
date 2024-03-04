f9f1540937320dd17823416d590fe56c3fc59496
Sai Dithvika <saidithvika@gmail.com>
2024-03-04 10:52:30 +0530

    ky

--- geofinance/src/pages/feautres.js
+++ geofinance/src/pages/feautres.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 import Header from '../Components/Header';
+import Feauture from '../Components/feauture';
  export default function Feautres(){
+  return(
+  <>
   <div>
     <Header></Header>
+    <Feauture></Feauture>
+   
     
   </div>
-
+  </>
+  )
  }
